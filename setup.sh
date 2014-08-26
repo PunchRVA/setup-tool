37d50c6d959f23519d4885ee71ab28545dc59b07
Chris May <chris@punchrva.com>
2014-08-26 11:01:02 -0400

    Removing unneeded line
    Signed-off-by: Chris May <chris@punchrva.com>

--- setup.sh
+++ setup.sh
@@ -11,10 +11,6 @@
 # 		pull h/u/p from config file in project
 # 	npm install (as needed)
 
-# getRepositories() {
-# 	curl --user chrisma:692b2b0c6872274106058d4b51c7552d6494b623528339f47f --user-agent "setup-tool" --header "Content-Type:application/json" https://punchrva.beanstalkapp.com/api/repositories.json > ~/.PUNCH/current_beanstalk_repos.json
-# 	cat ~/.PUNCH/current_beanstalk_repos.json
-# }
 
 # getRepositories
 git pull
